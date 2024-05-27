b0a8133a39913d05fc2c02e6669d467d5cf45793
Andy <aiurciuc@gmail.com>
2024-05-27 13:37:13 +0300

    MPL-1002 :  Add navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: () =>
+      import('./feature/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'planner',
+    title: 'Planner',
+    loadComponent: () =>
+      import('./feature/planner/planner.component').then(
+        (m) => m.PlannerComponent
+      ),
+  },
+  {
+    path: 'player',
+    title: 'Player',
+    loadComponent: () =>
+      import('./feature/player/player.component').then(
+        (m) => m.PlayerComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { LogoComponent } from '@shared/components/logo/logo.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,10 +14,12 @@ import { LogoComponent } from '@shared/components/logo/logo.component';
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
+    RouterLink, RouterLinkActive,
     LogoComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
   title = input.required<string>();
